07512a1d04e9dee4c00a9d879be7e7c1bfeb536a
Kevin Widjaja <kevin.kevinwidjaja@gmail.com>
2020-03-31 21:18:16 +0700

    Added Click logic and improvements to EmployeeMain's Navigation Drawer Added Fragments (CustomerList, EmployeeProfile) Improvements to LoginScreen UI

--- app/src/main/java/com/kevin/smartleasing/EmployeeMain.java
+++ app/src/main/java/com/kevin/smartleasing/EmployeeMain.java
@@ -1,12 +1,21 @@
 package com.kevin.smartleasing;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class EmployeeMain extends AppCompatActivity {
+public class EmployeeMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,46 @@ public class EmployeeMain extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        Tampilkan fragment Customer List saat pertama kali
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CustomerList()).commit();
+            navigationView.setCheckedItem(R.id.nav_customer_list);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+//        Bila back ditekan maka tutup dulu navigation drawer bila terbuka
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        Program untuk Navigation View, logika klik
+        switch (menuItem.getItemId()) {
+            case R.id.nav_customer_list:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CustomerList()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EmployeeProfile()).commit();
+                break;
+        }
+
+//        Tutup Drawer setelah klik
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/kevin/smartleasing/LoginScreen.java
+++ app/src/main/java/com/kevin/smartleasing/LoginScreen.java
@@ -1,12 +1,17 @@
 package com.kevin.smartleasing;
 
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,5 +33,13 @@ public class LoginScreen extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+//        Set background status bar to match logo color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
     }
 }
